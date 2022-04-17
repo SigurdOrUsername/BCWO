929713eb6e3a87337d3fa05e8063ef5563ce2d9d
SigurdOrUsername <97767396+SigurdOrUsername@users.noreply.github.com>
2022-04-17 04:35:36 +0200

    Update RasberryMain_OLD.lua

--- RasberryMain_OLD.lua
+++ RasberryMain_OLD.lua
@@ -407,6 +407,7 @@ RunService.Stepped:connect(function()
                             Player.Character.HumanoidRootPart.CFrame = MainPart.CFrame * CFrame.new(0, 500, 0)
 
                             task.spawn(function()
+                                task.wait()
                                 Tool.RemoteFunction:InvokeServer("shoot", {
                                     MainPart.CFrame,
                                     Tool.Damage.Value
@@ -415,10 +416,13 @@ RunService.Stepped:connect(function()
 
                         else
 
-                            Player.Character.HumanoidRootPart.CFrame = CFrame.lookAt(MainPart.Position + Vector3.new(0, 0, ToolLength * 1.2), MainPart.Position)
-                            Tool.Grip = CFrame.new(Player.Character.HumanoidRootPart.Position - MainPart.Position) - Vector3.new( Player.Character.HumanoidRootPart.Position.X - Player.Character["Right Arm"].Position.X, 0, 0)
+                            Player.Character.HumanoidRootPart.CFrame = CFrame.new(MainPart.Position + Vector3.new(0, 0, ToolLength * 1.2))
+
+                            local HumPos = Player.Character.HumanoidRootPart.Position
+                            Tool.Grip = CFrame.new(HumPos - MainPart.Position) - Vector3.new(HumPos.X - Player.Character["Right Arm"].Position.X, 0, MainPart.Size.Z * 2)
 
                             task.spawn(function()
+                                task.wait()
                                 Tool.RemoteFunction:InvokeServer("hit", {
                                     Tool.Damage.Value,
                                     0
